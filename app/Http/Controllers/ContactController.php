9028605fe579c02299b68d0944264c6fda311cbf
hakua <hakua411@gmail.com>
2026-08-28 20:06:31 +0900

    feat: エクスポート実装 #19

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -71,6 +71,60 @@ class ContactController extends Controller
         return view('admin.index', compact('contacts', 'categories', 'tags'));
     }
 
+    public function export(Request $request)
+    {
+        $query = Contact::with('category');
+
+        $query->keywordSearch($request->keyword);
+        $query->genderSearch($request->gender);
+        $query->categorySearch($request->category_id);
+        $query->dateSearch($request->date);
+
+        $contacts = $query
+        ->orderBy('created_at', 'desc')
+        ->get();
+
+        $csv = fopen('php://temp', 'r+');
+
+        fwrite($csv, "\xEF\xBB\xBF");
+
+        fputcsv($csv, [
+            'ID',
+            '氏名',
+            '性別',
+            'メール',
+            '電話',
+            '住所',
+            '建物',
+            'カテゴリ',
+            '内容',
+            '作成日時',
+        ]);
+
+        foreach ($contacts as $contact) {
+            fputcsv($csv, [
+                $contact->id,
+                $contact->fist_name . ' ' . $contact->last_name,
+                $contact->gender == 1 ? '男性' : ($contact->gender == 2 ? '女性' : 'その他'),
+                $contact->email,
+                $contact->tel,
+                $contact->address,
+                $contact->building,
+                $contact->category->content,
+                $contact->detail,
+                $contact->created_at,
+            ]);
+        }
+
+        rewind($csv);
+        $csvData = stream_get_contents($csv);
+        fclose($csv);
+
+        return response($csvData)
+        ->header('Content-Type', 'text/csv; charset=UTF-8')
+        ->header('Content-Disposition', 'attachment; filename="contacts.csv"');
+    }
+
     public function show(Contact $contact)
     {
         $contact->load('category', 'tags');
